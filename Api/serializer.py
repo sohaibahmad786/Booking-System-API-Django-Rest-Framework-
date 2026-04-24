3dd6f9cb1f7f12b38d9c02cb5f6814d638f09090
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-04-24 16:41:07 +0500

    Update serializer.py

--- Api/serializer.py
+++ Api/serializer.py
@@ -2,11 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.hashers import make_password, check_password
 
 from .models import Register
-from .models import Search_data
-from .models import Students
-from .models import Task
 from .models import Booking
-from .models import Message
         
 class Register_serializer(serializers.ModelSerializer):
     class Meta:
@@ -17,20 +13,6 @@ class Register_serializer(serializers.ModelSerializer):
         validated_data['password']=make_password(validated_data['password'])
         return super().create(validated_data)
 
-class Search_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Search_data
-        fields='__all__'
-
-class Student_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Students
-        fields='__all__'
-
-class Task_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Task
-        fields='__all__'
 
 class Booking_serializer(serializers.ModelSerializer):
     class Meta:
@@ -39,11 +21,7 @@ class Booking_serializer(serializers.ModelSerializer):
         extra_kwargs={
            'user':{'read_only':True}
         }
-
-class Message_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Message
-        fields='__all__'
+            
         extra_kwargs={
             'sender':{'read_only':True}
         }
