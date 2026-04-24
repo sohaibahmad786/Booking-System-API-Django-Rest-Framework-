6df76aea1c3735fdbdb2d785d7b08de536607a5b
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-04-24 16:47:03 +0500

    Update urls.py

--- Api/urls.py
+++ Api/urls.py
@@ -1,24 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from .views import Register_view,Register_detail
-from .views import Search_view
-from .views import Studentlist,Studentdetail
-from .views import Task_listview,Task_detailview
 from .views import Bookinglistview,AvailableSlotView
-from .views import MessagelistView,Chatlistview
 
 urlpatterns = [
    path('register/',Register_view.as_view()),
    path('register/<int:pk>/',Register_detail.as_view()),
    path('login/',TokenObtainPairView.as_view()),
    path('login/refresh/',TokenRefreshView.as_view()),
-   path('search/',Search_view.as_view()),
-   path('student/',Studentlist.as_view()),
-   path('student/<int:pk>/',Studentdetail.as_view()),
-   path('task/',Task_listview.as_view()),
-   path('task/<int:pk>/',Task_detailview.as_view()),
    path('book/',Bookinglistview.as_view()),
    path('available/',AvailableSlotView.as_view()),
-   path('message/',MessagelistView.as_view()),
-   path('chat/',Chatlistview.as_view()),
 ]
